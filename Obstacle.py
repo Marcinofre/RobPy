9ae821d13c848925e8a4e9ec5ea5485b840d9220
David <gudavid.1612@gmail.com>
2024-01-25 20:12:59 +0100

    Modification mise en forme d'Obstacle

--- Obstacle.py
+++ Obstacle.py
@@ -7,12 +7,12 @@ class Obstacle :
     def __init__(self, x1, x2, y1, y2) :
         """
         Constructeur d'un obstacle
-        arg x1 : Position en x de départ du vecteur
-        arg x2 : Position en x d'arrivée du vecteur
-        arg y1 : Position en y de départ du vecteur
-        arg y2 : Position en y d'arrivée du vecteur
+        arg x1 -> Position en x de départ du vecteur
+        arg x2 -> Position en x d'arrivée du vecteur
+        arg y1 -> Position en y de départ du vecteur
+        arg y2 -> Position en y d'arrivée du vecteur
 
         Attribut d'instance d'Obstacle :
-        VecteurObstacle : Création d'une instance de la classe Vecteur avec comme attribut en x : x2-x1, en y : y2-y1
+        VecteurObstacle -> Création d'une instance de la classe Vecteur avec comme attribut en x : x2-x1, en y : y2-y1
         """
         VecteurObstacle : Vecteur(x2-x1, y2-y1)
