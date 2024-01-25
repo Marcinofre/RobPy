602d0c5382f9b94e3250d0360a63f5fd9df3901f
David <gudavid.1612@gmail.com>
2024-01-25 19:51:09 +0100

    Ajout descriptif du constructeur d'Obstacle

---
+++ Obstacle.py
@@ -0,0 +1,12 @@
+import Vecteur from Vecteur
+class Obstacle :
+    """
+        Constructeur d'un obstacle
+        arg x1 : Position en x de départ du vecteur
+        arg x2 : Position en x d'arrivée du vecteur
+        arg y1 : Position en y de départ du vecteur
+        arg y2 : Position en y d'arrivée du vecteur
+
+        Attribut d'instance d'Obstacle :
+        VecteurObstacle : Création d'une instance de la classe Vecteur avec comme attribut en x : x2-x1, en y : y2-y1
+    """
