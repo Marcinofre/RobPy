18da69faf7d9e6892d85b6693f5106799a1e37af
Marcinofre <echecetmatayma@gmail.com>
2024-01-27 14:49:04 +0100

    Début d'écriture de la fonction test de avancerRobot

--- testRobot.py
+++ testRobot.py
@@ -1,4 +1,19 @@
 from Robot import Robot as r
-import unittest as un
+from Vecteur import Vecteur as vec
+import unittest
 
-class Robot(Test.TestCase)
+class Robot(unittest.TestCase) :
+	
+	def test_avancerRobot(self):
+		robot = r(0,0,0,0)
+		robot.vectD = vec(0.1, 0)
+		robot.vectV = vec(1,2)
+		
+		print(robot.vectV.calcNorm())
+		robot.avancerRobot()
+		print(robot.posCenter)
+		
+
+
+if __name__ == "__main__":
+	unittest.main() 
