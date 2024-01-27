231a1df3915d41ed52ad78a83b14c23e232edb4e
Yaniss Si Mohammed <yanisssimohammed@MacBook-Air-de-Yaniss.local>
2024-01-27 12:27:05 +0100

    Ajout de la fonction avancer dans la classe Robot et de l'attribut vectD  et modification de l'attribut vectV

--- Robot.py
+++ Robot.py
@@ -1,4 +1,6 @@
+from Vecteur import Vecteur
 from Moteur import Moteur
+
 class Robot :
 	"""
 		Modélisation d'un robot de Sorbonne Université
@@ -8,7 +10,11 @@ class Robot :
 		
 		self._dim = (width, length)
 		
-		#Position en x et y du centre du robot 
+		self.vectD = Vecteur(0, 0)  # Vecteur direction par défaut (0, 0)
+        
+		self.vectV = Vecteur(0, 0)  # Vecteur vitesse par défaut (0, 0)
+	
+		#Position en x et y du centre du robot
 		self.posCenter = (x,y)
 		
 		#Position globale du robot (devant, derriere, droit et gauche)
@@ -16,9 +22,6 @@ class Robot :
 		self.posRear = (x, y - length/2)
 		self.posLeftSide = (x - (width/2), y)
 		self.posRightSide = (x + (width/2), y)
-
-		#Vitesse du Robot (x, y)
-		self.vectV = (1,1)
 
 		#Ajout des Moteurs au robot (droite et gauche)
 		self._motorRight = Moteur("droit")
@@ -41,13 +44,13 @@ class Robot :
 		"""
 		self.posCenter = (self.posCenter[0] + self.vectV[0], self.posCenter[1] + self.vectV[1])
 
-
-
-
-	
-
-
-
+	def avancerRobot(self):
+		"""
+			Met à jour la position du robot vers l'avant en fonction du vecteur vitesse et direction
+		"""
+		self.posCenter=(self.posCenter[0]+self.vectV.x , self.posCenter[1]+self.vectV.y )
+    
+ 
 
 
 
