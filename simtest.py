a993132bc22d290db8737a817fbf690b3871cc16
Marcinofre <echecetmatayma@gmail.com>
2024-01-28 17:36:28 +0100

    script de test, pour tester les fonctions runAgent et associés

---
+++ simtest.py
@@ -0,0 +1,15 @@
+from Environnement import Environnement as env
+from Robot import Robot as rob
+from Vecteur import Vecteur as vect
+
+r = rob(0,0,0,0)
+e = env(100,100, r)
+r.vectD = vect(1,1)
+
+#Activation de l'agent
+e.runEnv()
+e.activateAgent(r)
+e.runAgent(r,"scriptRobot.txt")
+print(e.currentClock)
+print(r.posCenter)
+
