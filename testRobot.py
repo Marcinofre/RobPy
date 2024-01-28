8fb68da2c5e298d6809e91640f7304e389c19f95
Marcinofre <echecetmatayma@gmail.com>
2024-01-28 14:01:52 +0100

    2eme et 3eme test de avancerRobot()

--- testRobot.py
+++ testRobot.py
@@ -9,14 +9,24 @@ class Robot(unittest.TestCase) :
 		robot.vectD = vec(0.1, 0)
 		
 		
-		robot.scalVitesse= 2.0
+		robot.scalVitesse = 2.0
 		for time in range(10):
 			robot.avancerRobot()
 
 		self.assertEqual(robot.posCenter, (round(2.0*0.1*10, 2), 0.0))
 		
-		
-		self.assertEqual(robot.posCenter, (, 0.0))
+		robot.scalVitesse = 0
+		robot.posCenter = (0.0,0.0)
+		for time in range(10):
+			robot.avancerRobot()
+		self.assertEqual(robot.posCenter, (0.0, 0.0))
+
+		robot.scalVitesse = 15
+		robot.posCenter = (0.0,0.0)
+		robot.vectD = vec(0.1, -4)
+		for time in range(5):
+			robot.avancerRobot()
+		self.assertEqual(robot.posCenter, (7.5, -300.0))
 
 		
 
