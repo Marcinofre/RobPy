6373366fb4a4040ddb5b7d7220b873d1c11b1c95
David <gudavid.1612@gmail.com>
2024-02-02 17:06:35 +0100

    Robot au centre du canvas

--- Module/Interface.py
+++ Module/Interface.py
@@ -26,13 +26,20 @@ class Interface:
         self.framel.pack(side=tkinter.LEFT)
         self.framer = tkinter.Frame(self.fenetre, height = 720, width = 256, highlightbackground="black",highlightthickness=2)
         self.framer.pack(side=tkinter.RIGHT)
-        self.canvas = tkinter.Canvas(self.framel, width=width, height=height, bg = 'gray')
-        self.canvas.pack(side=tkinter.LEFT)
-        self.canvas.create_rectangle(-agent._dim[0]/2, -agent._dim[1]/2, agent._dim[0]/2, agent._dim[1]/2)
+        self.canvas = tkinter.Canvas(self.framel, width=1024, height=720, bg = 'gray')
+        self.canvas.pack()
+        rect_width = agent._dim[0]
+        rect_height = agent._dim[1]
+        x0 = (1024 - rect_width) / 2
+        y0 = (720 - rect_height) / 2
+        x1 = x0 + rect_width
+        y1 = y0 + rect_height
+        self.canvas.create_rectangle(x0, y0, x1, y1)
+        self.canvas.create_line(agent.posCenter[0], agent.posCenter[1], agent.posCenter[0]+agent.vectD.x, agent.posCenter[1]+agent.vectD.y)
 
     def ajoutObstacle(self, obs : Obstacle):
         """
-            Ajoute un obstacle dans l'interface graphique
+            Ajoute un obstacle dans l'interface graphique, lève une exception si il y a déjà un objet à cet endroit là
         """
         if self.canvas.find_overlapping(obs.x0, obs.y0, obs.x1, obs.y1):
             raise Exception("Il y a déjà un objet à cet endroit là")
