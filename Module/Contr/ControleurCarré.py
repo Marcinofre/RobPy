0bb7ae1a8015a29ea048fe1505bdd2b2eeb31cbb
David <gudavid.1612@gmail.com>
2024-02-11 19:50:39 +0100

    Modification ControleurCarré

--- Module/Contr/ControleurCarré.py
+++ Module/Contr/ControleurCarré.py
@@ -1,15 +1,29 @@
 from Module.Env.Environnement import Environnement
-from Module.Agent.Robot import Robot
-from AvancerDroit import AvancerDroit
+from Module.Contr.AvancerDroit import AvancerDroit
+from Module.Contr.TournerDroite import TournerDroite
 
 import time
 
-class RobotControlleur():
-    def __init__(self,Agent : Robot, env : Environnement):
-        self.Robot=Agent
-        self.env=env
+class ControleurCarré():
+    def __init__(self, env : Environnement):
+        distance = 10
+        self.strats = [AvancerDroit(distance, self.env), TournerDroite(90, self.env), AvancerDroit(distance, self.env), TournerDroite(90, self.env),AvancerDroit(distance, self.env), TournerDroite(90, self.env),AvancerDroit(distance, self.env), TournerDroite(90, self.env)]
+        self.env = env
+        self.cur = -1
     
+    def start(self):
+        self.cur = -1
 
+    def step(self):
+        if self.stop():
+            return
+        if self.cur<0 or self.strats[self.cur].stop():
+            self.cur+=1
+            self.strats[self.cur].step()
+    
+    def stop(self):
+        return self.curr ==len(self.strats)-1 and self.strats[self.cur].stop()
+    
     def avancer_robot(self,distance):
         strategie=AvancerDroit(distance,self.env)
         strategie.start()
