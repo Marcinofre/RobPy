920e2cbfc7433a0caf9a57d5bde9e1fbc62a750e
David <gudavid.1612@gmail.com>
2024-02-11 20:02:20 +0100

    Ajout documentation de ControleurCarré

--- Module/Contr/ControleurCarré.py
+++ Module/Contr/ControleurCarré.py
@@ -5,16 +5,34 @@ from Module.Contr.TournerDroite import TournerDroite
 import time
 
 class ControleurCarré():
+    """
+        Un controleur du robot dont le but est de lui faire tracer un carré
+    """
     def __init__(self, env : Environnement):
+        """
+            Constructeur de la classe ControleurCarré:
+            arg env : Environnement que le controleur a accès
+            
+            ---
+
+            Attributs d'instances :
+            strats = Liste comprenant des instances des classes AvancerDroit et TournerDroite, les instructions que le controleur enverra au Robot
+            cur = Index permettant de désigner l'instruction qui est en train d'éxécuter (Initialisé à -1 et va jusqu'à len de strats -1)
+        """
         distance = 10
-        self.strats = [AvancerDroit(distance, self.env), TournerDroite(90, self.env), AvancerDroit(distance, self.env), TournerDroite(90, self.env),AvancerDroit(distance, self.env), TournerDroite(90, self.env),AvancerDroit(distance, self.env), TournerDroite(90, self.env)]
-        self.env = env
+        self.strats = [AvancerDroit(distance, env), TournerDroite(90, env), AvancerDroit(distance, env), TournerDroite(90, env),AvancerDroit(distance, env), TournerDroite(90, env),AvancerDroit(distance, env), TournerDroite(90, env)]
         self.cur = -1
     
     def start(self):
+        """
+            Remet cur à -1 pour remettre le controleur sur la première instruction possible
+        """
         self.cur = -1
 
     def step(self):
+        """
+            Fonction qui parcours les instructions 
+        """
         if self.stop():
             return
         if self.cur<0 or self.strats[self.cur].stop():
@@ -22,9 +40,15 @@ class ControleurCarré():
             self.useStrat(self.strats[self.cur])
     
     def stop(self):
+        """
+            Condition d'arrêt de step
+        """
         return self.curr == len(self.strats)-1 and self.strats[self.cur].stop()
     
     def useStrat(self, strat):
+        """
+            Fonction permettant d'exécuter l'instruction désigné par step
+        """
         strat.start()
         while not strat.stop():
             strat.step()
