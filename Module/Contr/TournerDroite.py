173d612a3b5326fdc0162472a20e787076a3529e
David <gudavid.1612@gmail.com>
2024-02-13 16:26:45 +0100

    Complétion de la classe strat TournerDroite

--- Module/Contr/TournerDroite.py
+++ Module/Contr/TournerDroite.py
@@ -1,2 +1,24 @@
-class TournerDroite :
-    pass
+from Module.Env.Environnement import Environnement
+
+class TournerDroite():
+    """
+        Classe Strat de l'action de tourner à droite
+    """
+    def __init__(self, angle, env : Environnement):
+        self.angle = angle
+        self.env = env
+
+    def updateTime(self) :
+        return self.env.clockPace
+    
+    def start(self):
+        self.parcouru = 0
+
+    def step(self):
+        self.parcouru += 5
+        if self.stop() :
+            return
+        self.env.agent.rotateAllVect(5)
+
+    def stop(self):
+        return self.parcouru > self.angle
