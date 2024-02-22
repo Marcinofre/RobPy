5f0f258226f1cbb498d4b3fd0dbc43d93be2ed07
David <gudavid.1612@gmail.com>
2024-02-22 17:28:01 +0100

    Ajout threading et update dans le main

--- testInt.py
+++ testInt.py
@@ -5,6 +5,15 @@ from Module.Interface import Interface
 from Module.Env.Environnement import Environnement as env
 from Module.Contr.ControleurCarré import ControleurCarré as contrcar
 import time
+import threading
+
+def update():
+    while True :
+        if c.stop():
+            c.start()
+        c.step()
+        c.strats[c.cur].step()
+        time.sleep(1./e.clockPace)
 
 #Initialisation interface, Robot et Environnment
 r = Robot(30,40,180,180)
@@ -12,5 +21,6 @@ r.vectD = vect(0,-15)
 e = env(1024, 720, r)
 c = contrcar(r,e)
 sim = Interface(e,c)
+t = threading.Thread(target=update)
+t.start()
 sim.affiche()
-
