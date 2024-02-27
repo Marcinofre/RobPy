1f065e4a111d7057648393b036f95047c59d1583
Yaniss Si Mohammed <yanisssimohammed@MacBook-Air-de-Yaniss.local>
2024-02-27 09:06:37 +0100

    Ajout de commentaires pour le constructeur de la classe Capteur

--- Module/Agent/Capteur.py
+++ Module/Agent/Capteur.py
@@ -3,6 +3,14 @@ from Module.Vecteur import Vecteur
 class Capteur :
     """
 		Modélisation du capteur de mouvement du robot de Sorbonne Université
-	"""
+	"""()
 
-    def __init__(self, ray:int, vision:int, touchObstacle:bool=False ) -> None:
+    def __init__(self, ray:Vecteur, vision:int, touchObstacle:bool=False ) -> None:
+
+        """
+			Constructeur d'un capteur
+			Vecteur ray	: vecteur représentant le rayon du capteur
+			int vision	: distance de vision du capteur
+            boolean touchObstacle : indique si un obstacle touche le vecteur   -> False ( aucun obstacle )
+                                                                               -> True ( obsctale)
+	    """
