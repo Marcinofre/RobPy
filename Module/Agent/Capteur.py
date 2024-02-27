7db53a60d07e265e39d02ea4de69f1e9b50786e8
Yaniss Si Mohammed <yanisssimohammed@MacBook-Air-de-Yaniss.local>
2024-02-27 10:18:04 +0100

    Ajout de commentaires dans la classe Capteur

--- Module/Agent/Capteur.py
+++ Module/Agent/Capteur.py
@@ -28,23 +28,33 @@ class Capteur :
         """
         return (self.ray.x * distance, self.ray.y * distance)
     
+    
     def doesRayTouch (self,ray):
         """
             Retourne un boolean qui dis si le rayon touche un obstacle
         """
-        # A faire
+        # =======> A faire
+        
 
     def RayTouchObsctacle (self):
         """
             Projection du rayon qui s'incrémente jusqu'à détécter un obstacle
         """
-        projection = 0.01 # Initialisition de la projection du rayon à 1cm
+        # Initialisition de la projection du rayon à 1cm
+        projection = 0.01 
+        # Tant que la projection est dans la vision et qu'aucun obstacle ne touche le vecteur du rayon 
         while (self.vision < projection) and ( not self.agent.capteur.touchObstacle): 
+            # Si un obstacle est détécté 
             if self.doesRayTouch(self.ray):
+                # On sort de la boucle en mettant à jour l'attribut touchObstacle
                 self.agent.capteur.touchObstacle = True
+                # On retourne la projection pour connaître la distance entre le robot et l'obstacle
                 return projection
+            # Si aucun obstacle est détécté
             else:
-                projection = projection + 0.01  # Incrémentation de la projection du rayon de 1cm
+                # Incrémentation de la projection du rayon de 1cm
+                projection = projection + 0.01  
+                # Mise à jour de la projection du rayon du capteur
                 self.agent.capteur.projectionRay(projection)
         return -1
 
