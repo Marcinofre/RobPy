074c43af308bf0524b3a930b3a3801f07afa5ebf
Yaniss Si Mohammed <yanisssimohammed@MacBook-Air-de-Yaniss.local>
2024-02-27 08:47:50 +0100

    Ajout du constructeur de Capteur

--- Module/Agent/Capteur.py
+++ Module/Agent/Capteur.py
@@ -4,3 +4,5 @@ class Capteur :
     """
 		Modélisation du capteur de mouvement du robot de Sorbonne Université
 	"""
+
+    def __init__(self, ray:int, vision:int = 1, touchObstacle:bool=False ) -> None:
