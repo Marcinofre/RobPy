d0b58f9ad321490a16d5c935260786c00e513f4a
Yaniss Si Mohammed <yanisssimohammed@MacBook-Air-de-Yaniss.local>
2024-02-27 09:35:09 +0100

    Ajout du principe de la fonction projectionRay

--- Module/Agent/Capteur.py
+++ Module/Agent/Capteur.py
@@ -9,16 +9,23 @@ class Capteur :
 
         """
 			Constructeur d'un capteur =
-			    => int vision : distance de vision du capteur
+			    => int vision : distance de vision du capteur en mètres
 
             Attributs d'un capteur =
                 => Vecteur ray : vecteur représentant le rayon du capteur
 			    => int vision : distance de vision passée en paramètre du constructeur
                 => boolean touchObstacle : indique si un obstacle touche le vecteur   -> False ( aucun obstacle )
-                                                                               -> True ( obsctale)
+                                                                                      -> True ( obsctale)
 	    """
 
         self.ray = Vecteur(0, 0) 
         self.vision = vision
         self.touchObstacle = False
+
+    def projectionRay (self,distance):
+        """
+            Retourne le rayon projeté à la distance passé en paramètre en mètres
+        """
+       
+
     
