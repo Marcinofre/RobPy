f918c35d4b9939513a85e831fd1f874b28769d88
Yaniss Si Mohammed <yanisssimohammed@MacBook-Air-de-Yaniss.local>
2024-02-27 09:27:43 +0100

    Ajout des attributs de Capteur

--- Module/Agent/Capteur.py
+++ Module/Agent/Capteur.py
@@ -3,16 +3,22 @@ from Module.Vecteur import Vecteur
 class Capteur :
     """
 		Modélisation du capteur de mouvement du robot de Sorbonne Université
-	"""()
+	"""
 
     def __init__(self, vision:int) -> None:
 
         """
-			Constructeur d'un capteur
-			Vecteur ray	: vecteur représentant le rayon du capteur
-			int vision	: distance de vision du capteur
-            boolean touchObstacle : indique si un obstacle touche le vecteur   -> False ( aucun obstacle )
+			Constructeur d'un capteur =
+			    => int vision : distance de vision du capteur
+
+            Attributs d'un capteur =
+                => Vecteur ray : vecteur représentant le rayon du capteur
+			    => int vision : distance de vision passée en paramètre du constructeur
+                => boolean touchObstacle : indique si un obstacle touche le vecteur   -> False ( aucun obstacle )
                                                                                -> True ( obsctale)
 	    """
 
-        
+        self.ray = Vecteur(0, 0) 
+        self.vision = vision
+        self.touchObstacle = False
+    
