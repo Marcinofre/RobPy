be68db893ad43d215e972386261a82e45e79ea21
Marcinofre <echecetmatayma@gmail.com>
2024-03-03 16:44:08 +0100

    Ajout de condition pour terminer la simulation proprement dans la version terminal de la simulation

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ from Module.Vecteur import Vecteur as vect
 from Module.Agent.Robot import Robot
 from Module.Interface import Interface
 from Module.Env.Environnement import Environnement as env
-from Module.Contr.ControleurCarre import ControleurCarre
+#from Module.Contr.ControleurCarre import ControleurCarre
 from Module.Contr.AvancerSansCollision import AvancerSansCollision
 from Module.Env.Obstacle import Obstacle
+
 import time
 import threading
 
+
 def introductionSimulation():
+    """
+        Affiche une introduction et retourne un bool selon si la simulation doit etre demarrer avec ou sans interface graphique depuis le terminal
+    """
     print("The RobPy simulation is currently being initialized.")
     print("Please kindly follow the instructions that may follow")
     print("For each instruction, please write \"yes\" or (\"y\") or \"no\" (\"n\"). If another element is detected, it will be considered \"no\" as default")
@@ -26,7 +31,7 @@ def updateEnv(env):
     """
     env.initSimulation()
     while env.isRunning :
-        env.update()			#---> Update l'environnement
+        env.update()			        #---> Update l'environnement
         time.sleep(1./env.clockPace)	#---> frame par sec 
 
 def updateContr(env, contr):
@@ -39,14 +44,20 @@ def updateContr(env, contr):
             contr.strats[contr.cur].step()
         time.sleep(1./env.clockPace)
 
+######################################################################################################################################
+        
 
 ##Script de lancement de la simulation
 
 interfaceOn = introductionSimulation()
 
 print("Initialization in progress")
+
+
 #Définition de la taille de l'environnment
 taille = (1024, 720)
+
+
 #Définition du vecteur directeur initial du robot, puis du robot
 vecteurDirecteur = vect(0,-15)
 robot = Robot(30,40,taille[0]*0.5,taille[1]*0.5,vecteurDirecteur)
@@ -65,8 +76,29 @@ if interfaceOn :
 environnement.addObstacle(Obstacle(400,200,600,200))
 updateE = threading.Thread(target=updateEnv, args=(environnement,))
 updateC = threading.Thread(target=updateContr, args=(environnement, controleurRobot))
-updateE.start()
-updateC.start()
 
+
+#Lancemement de la simulation SANS interface
+if not interfaceOn:
+    
+    #Lancement des threads
+    updateE.start()
+    updateC.start()
+    
+    while True:
+        input('--->>>>Press any key to stop<<<<---\n\n\n')
+        environnement.isRunning = False
+        break
+
+#Lancement de la simulation AVEC interface
 if interfaceOn:
+    
+    #Lancement des threads
+    updateE.start()
+    updateC.start()
+
+    #Affichage de la fenetre
     sim.affiche()
+
+
+ 
