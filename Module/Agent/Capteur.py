e3d0fd45721efea3f954ea67155b009827e5bf8c
Yaniss Si Mohammed <yanisssimohammed@MacBook-Air-de-Yaniss.local>
2024-03-04 13:09:25 +0100

    Ajout de commentaire

--- Module/Agent/Capteur.py
+++ Module/Agent/Capteur.py
@@ -6,11 +6,10 @@ class Capteur :
 	"""
 
     def __init__(self, vecteurDirecteurRobot:Vecteur) -> None:
-
         """
              Attributs d'un capteur
                 => Vecteur ray : vecteur représentant le rayon du capteur
-			    => int vision : distance de vision passée en paramètre du constructeur
+			    => int vision : distance de vision max
                 => boolean touchObstacle : indique si un obstacle touche le vecteur   -> False ( aucun obstacle )                                                                                                                                       -> True ( obsctale)
 	    """
         self.ray = Vecteur(0,1)
@@ -19,8 +18,11 @@ class Capteur :
         self.touchObstacle = False
         self.distanceObstacle = 0
 
-    def getObstacle(self):
 
+    def getObstacle(self):
+        """
+            Retourne la distance entre le robot et l'obstacle si il y'en a un, sinon retourne -1
+        """
         if self.e.retourCapteur(self.pas_distance):
             print(f"Stopped by obstacle ? : {self.r.capteur.touchObstacle}")
             print(f"Stopped by vision_max ? : {self.r.capteur.vision < self.distance_vue}")	
