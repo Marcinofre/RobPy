c3120e1cba0b653344539b139aa79490db4acb8c
David <gudavid.1612@gmail.com>
2024-03-05 21:23:04 +0100

    Modification ASC pour permettre de rfreiner et ne pas toucher le mur

--- Module/Contr/AvancerSansCollision.py
+++ Module/Contr/AvancerSansCollision.py
@@ -17,6 +17,7 @@ class AvancerSansCollision():
             strats = Liste comprenant des instances des classes AvancerDroit et TournerDirecte, les instructions que le controleur enverra au Robot
             cur = Index permettant de désigner l'instruction qui est en train d'éxécuter (Initialisé à -1 et va jusqu'à len de strats -1)
         """
+        self.env = en
         self.robot = robot                          # ---> Robot à controler
         self.distance = 0                           # ---> Distance à parcourir
         self.speed = 0.6                            # ---> Vitesse du robot initial
@@ -38,25 +39,13 @@ class AvancerSansCollision():
 
         if self.stop():
             return
-        
-        #Update de la distance en fonction de la distance calculer par rapport à l'obstacle
         self.distance = self.robot.capteur.distanceObstacle
-
-        #Calcule de la dist pour le freinage par rapport à la longeur frontale du robot
-        dist = self.distance - self.robot._dim[1]
+        if self.distance <100:
+            if self.speed != 0:
+                self.speed = round(self.speed * self.distance*0.01, 2)  # On applique le freinage à la roue en évitant les valeures absurdes
+        self.robot.setVitesseRoue(self.speed, self.speed) 
         
-        #Systeme de frein/decceleration
-        if dist < 0 and self.distance != 0:
-            new_speed = self.speed*abs(dist)/self.distance
-            if self.speed > 0:
-                self.speed -= round(new_speed, 2)           # ---> Arrondie pour eviter d'avoir des chiffres un peu absurde
-            else:
-                self.speed = 0
-
-        #Modification de la vitesse des roues puis avance selon la vitesse moyenne
-        self.robot.setVitesseRoue(self.speed, self.speed)
-        
-        print("Distance entre le robot et le mur",self.robot.capteur.distanceObstacle)
+        print(" Distance entre le robot et l'obstacle ",self.robot.capteur.distanceObstacle)
         
         if self.cur<0 or self.strats[self.cur].stop():
             self.cur+=1
