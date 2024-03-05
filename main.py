3c00facde8052fd4796263458644776758c24201
Marcinofre <echecetmatayma@gmail.com>
2024-03-05 20:11:25 +0100

    Commentaire pour la fonction draw_path

--- main.py
+++ main.py
@@ -82,9 +82,9 @@ controleurCarre = ControleurCarre(robot)
 #Initialisation de l'interface graphique et lancement
 if interfaceOn :
     sim = Interface(environnement,controleurCarre)
-    #sim.ajoutObstacle(Obstacle(400,200,600,180))
+    sim.ajoutObstacle(Obstacle(400,200,600,180))
 
-#environnement.addObstacle(Obstacle(400,200,600,200))
+environnement.addObstacle(Obstacle(400,200,600,200))
 updateE = threading.Thread(target=updateEnv, args=(environnement,))
 updateC = threading.Thread(target=updateContr, args=(environnement, controleurCollision))
 
