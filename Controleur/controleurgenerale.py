dd691cc62fceb9084c89a47d833781a3dd848ad0
Ynl4881 <108008411+Ynl4881@users.noreply.github.com>
2024-03-22 16:16:57 +0100

    Test

---
+++ Controleur/controleurgenerale.py
@@ -0,0 +1,14 @@
+from Agent.robot import Robot as rob
+from Controleur.avancerDroit import AvancerDroit
+from Controleur.tournerDirecte import TournerDirecte
+import time
+
+class controleurgenerale:
+    def __init__(self,Robot:rob) -> None:
+        self.robot=Robot
+
+    def carre():
+        pass
+
+    def collision():
+        pass
