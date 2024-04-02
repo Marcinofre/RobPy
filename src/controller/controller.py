5912727b411a23b0e196afa2fa6e9dcdc0f5c5ab
David <gudavid.1612@gmail.com>
2024-04-02 19:37:11 +0200

    Changement ControleurGenerique en SequentialController

--- src/controller/controller.py
+++ src/controller/controller.py
@@ -1,40 +1,35 @@
-class ControleurGenerique():
-    
-    def __init__(self, strats):
-        """
-            Constructeur de la classe ControleurCarré:
-            arg strats : Toutes les stratégies utilisés par le controleur
-            
-            ---
+from src.controller.strategies.unitstrats import unitStrat
 
-            Attributs d'instances :
-            strats = Liste comprenant toutes les stratégies suivi par le controleur
-            cur = Index permettant de désigner l'instruction qui est en train d'éxécuter (Initialisé à -1 et va jusqu'à len de strats -1)
-        """
-        self.strats = strats
-        self.cur = -1
-        self.speed = 1
+class SequentialController():
+	
+	def __init__(self, strats: list[unitStrat]) -> None:
+		"""
+		"""
+		self.strats = strats
+		self.cur = -1
 
-    def start(self):
-        """
-            Remet cur à -1 pour remettre le controleur sur la première instruction possible
-        """
-        self.cur = -1
-        for i in self.strats :
-            i.speed = self.speed
-            i.start()
+	def start(self) -> None:
+		"""Remet cur à -1 pour remettre le controleur sur la première instruction possible
+		"""
+		self.cur = -1
+		for strategy in self.strats :
+			strategy.start()
 
-    def step(self):
-        """
-            Fonction qui parcours les instructions 
-        """
-        if self.stop():
-            return
-        if self.cur<0 or self.strats[self.cur].stop():
-            self.cur+=1
-    
-    def stop(self):
-        """
-            Condition d'arrêt de step
-        """
-        return self.cur == len(self.strats)-1 and self.strats[self.cur].stop()
+	def step(self) -> None:
+		"""Parcours des instructions de la liste self.strats
+		"""
+		
+		
+		if self.cur<0 or self.strats[self.cur].stop():
+			print("Passage à la stratégie suivante")
+			if self.stop():
+				print("Fin de l'ensemble des stratégies")
+				return
+			self.cur += 1
+		else:
+			self.strats[self.cur].step()
+		
+	def stop(self) -> bool:
+		"""Condition d'arrêt de step
+		"""
+		return self.cur == len(self.strats)-1 and self.strats[self.cur].stop()
