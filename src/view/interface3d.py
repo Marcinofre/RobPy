754c79728b4fa52090d45eaa5c36468197454dcc
David <gudavid.1612@gmail.com>
2024-04-21 15:09:53 +0200

    Debut creation interface3d

--- src/view/interface3d.py
+++ src/view/interface3d.py
@@ -1,10 +1,20 @@
 from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
+from direct.actor.Actor import Actor
 
 
 class Interface3D(ShowBase):
 
     def __init__(self):
         ShowBase.__init__(self)
+        self.scene = self.loader.loadModel("assets/Environnement.glb")
+        self.scene.reparentTo(self.render)
+        self.scene.setScale(0.25, 0.25, 0.25)
+        self.scene.setPos(-8, 42, 0)
+        self.robot = self.loader.loadModel("assets/Robot.glb")
+        self.robot.setScale(10, 10, 10)
+        self.robot.setPos(-4,40,4)
+        self.robot.reparentTo(self.render)
 
 app = Interface3D()
 app.run()
