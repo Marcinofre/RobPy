74cf92e46cc3ba05ca71ecc96691d1ce86877b59
Marcinofre <echecetmatayma@gmail.com>
2024-04-24 00:18:02 +0200

    Refacto : Modification du lanceur de stratégie (légère modification de la condition d'arret trop qui coupait trop tot la step)

--- src/controller/seqstrat.py
+++ src/controller/seqstrat.py
@@ -1,5 +1,19 @@
 # -IMPORT ZONE---------------------------------------------------------------------------
 from src.controller.strategies.unitstrats import UnitStrat
+import logging
+# ---------------------------------------------------------------------------------------
+
+# -Logging setup-------------------------------------------------------------------------
+
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter('INFO : %(message)s')
+
+# Niveau de logging
+logger.setLevel(logging.INFO)
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(stream_handler)
 # ---------------------------------------------------------------------------------------
 
 # -CONTROLLER----------------------------------------------------------------------------
@@ -21,10 +35,10 @@ class SequentialStrategy(UnitStrat):
 	def step(self) -> None:
 		"""Parcours des instructions de la liste self.strats
 		"""
-		if self.cur<0 or self.strats[self.cur].stop():
-			print("Passage à la stratégie suivante")
+		if self.cur<0 or self.strats[self.cur].stop_strat:
+			logger.info("Passage à la stratégie suivante")
 			if self.stop():
-				print("Fin de l'ensemble des stratégies")
+				logger.info("Fin de l'ensemble des stratégies")
 				return
 			self.cur += 1
 		else:
@@ -33,4 +47,4 @@ class SequentialStrategy(UnitStrat):
 	def stop(self) -> bool:
 		"""Condition d'arrêt de step
 		"""
-		return self.cur == len(self.strats)-1 and self.strats[self.cur].stop()
+		return self.cur == len(self.strats)-1 and self.strats[self.cur].stop_strat
